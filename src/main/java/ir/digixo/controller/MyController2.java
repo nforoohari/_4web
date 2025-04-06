0c541407f9a48293fe156e4d336ff1b3a062023a
Nima Foroohari <nforoohari@gmail.com>
2025-04-06 10:44:45 +0330

    Version 0

---
+++ src/main/java/ir/digixo/controller/MyController2.java
@@ -0,0 +1,16 @@
+package ir.digixo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//servlet  ===> servlet  ===
+//@Component
+@Controller
+@RequestMapping("/home")
+public class MyController2 {
+
+    void  m()
+    {
+
+    }
+}
