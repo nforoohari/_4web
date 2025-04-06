537cab34ac0185d2feff64841a40f4edb98a33c8
Nima Foroohari <nforoohari@gmail.com>
2025-04-07 00:27:12 +0330

    Version 1

--- src/main/java/ir/digixo/controller/MyController.java
+++ src/main/java/ir/digixo/controller/MyController.java
@@ -1,26 +1,22 @@
 package ir.digixo.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //servlet  ===> servlet  ===
-//@Component
 @Controller
 @RequestMapping("/home")
 public class MyController {
 
-    @RequestMapping(value = "/service1",method = RequestMethod.GET)
-    String  m()
-    {
+    @RequestMapping(value = "/service1", method = RequestMethod.GET)
+    String m() {
 
         //logic  ===> service()
         return "home";
     }
-    void  m2()
-    {
+
+    void m2() {
 
     }
 }
